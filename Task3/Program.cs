8ac1c52da5b0634b432df085b285a5f28c48c7f3
StalikVtk <StalikVtk@yandex.ru>
2025-04-21 23:46:56 +0400

    Практическое задание № 3. Приветствие пользователя.

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+
+            Console.Write("Как Вас зовут? ");
+            myName = Console.ReadLine();
+
+            Console.WriteLine($"Добро пожаловать к нам: {myName}");
+            Console.ReadKey();
+        }
+    }
+}
